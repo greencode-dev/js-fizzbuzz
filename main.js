9310e03c86454cd920c0c4db97956b0bbbad5a37
greencode-dev <antonioverde.dev@gmail.com>
2025-11-04 16:43:35 +0100

    html print

--- main.js
+++ main.js
@@ -12,3 +12,18 @@ for (let i = 1; i <= 100; i++) {
 
   console.log(msg);
 }
+
+/* Stampa in HTML*/
+for (let i = 1; i <= 100; i++) {
+  let contenuto = i;
+
+  if (i % 3 == 0 && i % 5 == 0) {
+    contenuto = `FizzBuzz`;
+  } else if (i % 3 == 0) {
+    contenuto = `Fizz`;
+  } else if (i % 5 == 0) {
+    contenuto = `Buzz`;
+  }
+
+  document.getElementById('risultato').innerHTML += `<li>${contenuto}</li>`;
+}
